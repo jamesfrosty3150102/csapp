c5277a196c8efd886134a9867da7b4fb98993545
ywang43 <james.frosty3150102@gmail.com>
2021-12-12 11:29:38 +0800

    BackgroundworkerTest3

---
+++ BackgroundworkerTest3/BackgroundworkerTest3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundworkerTest3
+{
+    static class BackgroundworkerTest3
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
