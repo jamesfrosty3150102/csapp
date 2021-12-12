4bfa39a49c5e00cc481c17a3997d8420ce2da7d9
ywang43 <james.frosty3150102@gmail.com>
2021-12-12 16:56:47 +0800

    RandomTest

---
+++ RandomTest/RandomTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class RandomTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
