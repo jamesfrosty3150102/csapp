cb4f03b78e9b15855057cfbcc04b3b91a1f57289
ywang43 <james.frosty3150102@gmail.com>
2021-12-14 22:06:51 +0800

    StartsWithTest1

---
+++ StartsWithTest1/StartsWithTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StartsWithTest1
+{
+    class StartsWithTest1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
