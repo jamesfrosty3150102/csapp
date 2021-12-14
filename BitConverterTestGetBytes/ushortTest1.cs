ba6c7a21168653629ed3c5cafda7b525234a76ae
ywang43 <james.frosty3150102@gmail.com>
2021-12-14 21:45:33 +0800

    BitConverter.GetBytes

--- BitConverterTestGetBytes/ushortTest1.cs
+++ BitConverterTestGetBytes/ushortTest1.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ushortTest1
+namespace ushortTest2
 {
-    class Program
+    class ushortTest2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ushort abc = 0xFF;
+            Console.WriteLine(abc);
+            byte[] abc1 = null;
+            abc1 = BitConverter.GetBytes(abc);
+            for (int i = 0; i < abc1.Length; i++)
+            {
+                Console.WriteLine(abc1[i].ToString("X"));
+            }
         }
     }
 }
