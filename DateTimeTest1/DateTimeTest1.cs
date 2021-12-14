9bbe7c46254acf8d31e66e88e3f458c462db7f76
ywang43 <james.frosty3150102@gmail.com>
2021-12-14 21:35:09 +0800

    DateTimeTest

---
+++ DateTimeTest1/DateTimeTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateTimeTest1
+{
+    class DateTimeTest1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
