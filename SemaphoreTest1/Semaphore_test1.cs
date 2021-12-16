50e540fc5d9fbf91e251d6724932510e319f04e7
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2021-12-16 21:43:52 +0800

    SemaphoreTest1

--- SemaphoreTest1/Semaphore_test1.cs
+++ SemaphoreTest1/Semaphore_test1.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading;
 
-namespace Semaphore_test1
+namespace SemaphoreTest
 {
-    class Program
+    class Semaphore1
     {
-        static void Main(string[] args)
+        private static Semaphore _pool;
+        private static int _padding;
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            _pool = new Semaphore(0, 3);
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(Worker));
+                t.Start(i);
+            }
+            Thread.Sleep(500);
+            Console.WriteLine("Main thread calls Release(3).");
+            _pool.Release(3);
         }
-    }
+        private static void Worker(object num)
+        {
+            Console.WriteLine("Worker");
+            _pool.WaitOne();
+            int padding = Interlocked.Add(ref _padding, 100);
+            Console.WriteLine("Thread {0} enters the semaphore.", num);
+
+            Thread.Sleep(1000 + padding);
+            Console.WriteLine("Thread {0} releases the semaphore.", num);
+            Console.WriteLine("Thread {0} previous semaphore count: {1}",
+                num, _pool.Release());
+        }
+
+        }
 }
