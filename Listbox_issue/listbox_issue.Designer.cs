1e7caf94c0098c5367bf3572a868d756dfee69a2
ywang43 <james.frosty3150102@gmail.com>
2022-08-27 12:24:54 +0800

    reading data

--- Listbox_issue/listbox_issue.Designer.cs
+++ Listbox_issue/listbox_issue.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Winform0827
 {
-    partial class Form1
+    partial class listbox_issue
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,13 +42,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(86, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(431, 274);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listbox_issue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "listbox_issue";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Listbox_issue/listbox_issue.cs
+++ Listbox_issue/listbox_issue.cs
@@ -7,20 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 
 namespace Winform0827
 {
-    public partial class Form1 : Form
+    public partial class listbox_issue : Form
     {
-        public Form1()
+        public listbox_issue()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            StreamReader _reader = new StreamReader(Application.StartupPath + "\\" + "data.csv");
+            string _line = _reader.ReadLine();
+            string[] _tempArray = null;
+            int _count = 0;
+            while(!_reader.EndOfStream)
+            {
+                _tempArray = _reader.ReadLine().Split(',');
+                Console.WriteLine(_tempArray[0]);
+                _count++;
+            }
         }
     }
 }
