ea4feff7ec01955cc0997466728918566758862e
ywang43 <james.frosty3150102@gmail.com>
2022-08-28 08:07:43 +0800

    No progressiveBar displaying

--- Listbox_issue/listbox_issue.Designer.cs
+++ Listbox_issue/listbox_issue.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this._bw = new System.ComponentModel.BackgroundWorker();
+            this.ckBGW = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -49,7 +52,7 @@
             this.listBox1.ItemHeight = 18;
             this.listBox1.Location = new System.Drawing.Point(66, 45);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(431, 274);
+            this.listBox1.Size = new System.Drawing.Size(447, 310);
             this.listBox1.TabIndex = 1;
             // 
             // progressBar1
@@ -59,17 +62,45 @@
             this.progressBar1.Size = new System.Drawing.Size(776, 29);
             this.progressBar1.TabIndex = 2;
             // 
+            // _bw
+            // 
+            this._bw.DoWork += new System.ComponentModel.DoWorkEventHandler(this._bw_DoWork);
+            this._bw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this._bw_ProgressChanged);
+            this._bw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this._bw_RunWorkerCompleted);
+            // 
+            // ckBGW
+            // 
+            this.ckBGW.AutoSize = true;
+            this.ckBGW.Location = new System.Drawing.Point(540, 128);
+            this.ckBGW.Name = "ckBGW";
+            this.ckBGW.Size = new System.Drawing.Size(89, 22);
+            this.ckBGW.TabIndex = 3;
+            this.ckBGW.Text = "chBGW";
+            this.ckBGW.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
             // listbox_issue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ckBGW);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "listbox_issue";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,6 +109,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker _bw;
+        private System.Windows.Forms.CheckBox ckBGW;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Listbox_issue/listbox_issue.cs
+++ Listbox_issue/listbox_issue.cs
@@ -20,10 +20,13 @@ namespace Winform0827
         int g_cycleTestCount = 0;
         //Get Data Cnt
         int g_sbsCount = 0;
+        //
 
         public listbox_issue()
         {
             InitializeComponent();
+            InitializeBackgroundWorker();
+            BGW();
             GetDataCnt();
         }
 
@@ -53,26 +56,97 @@ namespace Winform0827
             {
                 Task _taskRun = new Task(readingTask);
                 _taskRun.Start();
-                g_cycleTaskStart = true;         
+                g_cycleTaskStart = true;
             }
         }
         #endregion
 
         #region readingTask
         private void readingTask()
-        { 
+        {
             StreamReader _reader = new StreamReader(Application.StartupPath + "\\" + @"data.csv");
             string _line = _reader.ReadLine();
             string[] _tempArray = null;
             int _count = 0;
-            while(!_reader.EndOfStream)
+            while (!_reader.EndOfStream)
             {
                 _tempArray = _reader.ReadLine().Split(',');
                 Console.WriteLine(_tempArray[0]);
                 listBox1.Invoke(new Action(() => listBox1.Items.Add(_tempArray[0])));
                 _count++;
-            }        
+            }
         }
         #endregion readingTask
+
+
+        #region Backgroundworker
+        private void BGW()
+        {
+            ckBGW.Checked = true;
+            if (ckBGW.Checked)
+            {
+                _bw.WorkerReportsProgress = true;
+                _bw.WorkerSupportsCancellation = true;
+            }
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            _bw.DoWork += new DoWorkEventHandler(_bw_DoWork);
+            _bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_bw_RunWorkerCompleted);
+            _bw.ProgressChanged += new ProgressChangedEventHandler(_bw_ProgressChanged);
+        }
+
+        private void _bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            if (this.progressBar1.Value == 0)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (worker.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    else
+                    {
+                        // Perform a time consuming operation and report progress.
+                        System.Threading.Thread.Sleep(500);
+                        worker.ReportProgress(i * 10);
+                    }
+                    Console.WriteLine("this.progressBar1.Value:{0}", this.progressBar1.Value);
+                    //label1.Text = Convert.ToString(this.progressBar1.Value)+ "%";
+                }
+            }
+            Console.WriteLine("this.progressBar1.Value:{0}", this.progressBar1.Value);
+        }
+
+        private void _bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void _bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                //resultLabel.Text = "Canceled!";
+                //label1.Text = "Cancelled";
+                label1.Text = "Canceled!";
+            }
+            else if (e.Error != null)
+            {
+                //resultLabel.Text = "Error: " + e.Error.Message;
+                label1.Text = "Error:" + e.Error.Message;
+            }
+            else
+            {
+                //resultLabel.Text = "Done!";
+                //label1.Text = "100%";
+                label1.Text = "100%";
+            }
+        }
+        #endregion 
     }
 }
