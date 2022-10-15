16349ce5ec9c616cdb236bb926a9577bc4ca4628
ywang43 <james.frosty3150102@gmail.com>
2022-10-15 15:57:44 +0800

    Microsoft Doc

---
+++ Path_CS/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PathClassLibrary1;
+using System.IO;
+
+namespace PathMethodTests
+{
+    public partial class Form1 : Form
+    {
+        PathClass1 PathC = new PathClass1();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("ChangeExtension");
+            comboBox1.Items.Add("Combine");
+            comboBox1.Items.Add("GetDirectoryName");
+            comboBox1.Items.Add("GetExtension");
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string path1 = @"C:\temp\MyTest.txt";
+            string path2 = @"C:\temp\MyTest";
+            string path3 = @"temp";
+            string path4 = @"D:\CSharp\TEMP\W896001DA.TxT";
+            string path5 = "";
+
+            bool result = false;
+            int selectedIndex = comboBox1.SelectedIndex;
+            path1 = textBox1.Text;
+
+            switch (selectedIndex)
+            { 
+                case 0:
+                    result = PathC._ChangeExtension(ref path1);
+                    break;
+                case 1:
+                    result = PathC._Combine(ref path1);
+                    break;
+                case 2:
+                    result = PathC._EndsInDirectorySeparator(ref path1);
+                    break;
+                case 3:
+                    result = PathC._GetExtension(ref path4, ref path5);
+                    Console.WriteLine(path5);
+                    break;
+            }
+#if debug
+            bool result = false;
+            string data = "ABC";
+            result = PathC.dllTestAPI(ref data);
+            if (!result)
+            {
+                Console.WriteLine("testing failed");
+            }
+#endif
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = comboBox1.SelectedIndex;
+            Console.WriteLine(selectedIndex);
+            label1.Text = comboBox1.Items[1].ToString();
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Title = "選擇檔案";
+            od.Multiselect = true;
+            od.RestoreDirectory = true;
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                string testingPath = Path.GetDirectoryName(od.FileName);
+                textBox1.Text = testingPath;
+            }
+        }
+    }
+}
---
+++ Path_CS/PathClass1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PathClassLibrary1
+{
+    public class PathClass1
+    {
+        public bool dllTestAPI(ref string sMsg)
+        {
+            Console.WriteLine("dllTestAPI:{0}", sMsg);
+            return true;
+        }
+
+        public bool _ChangeExtension(ref string sMsg)
+        {
+            Console.WriteLine("ChangeExtension:{0}", sMsg);
+            return true;
+        }
+
+        public bool _Combine(ref string sMsg)
+        {
+            Console.WriteLine("Combine:{0}", sMsg);
+            return true;
+        }
+
+        public bool _EndsInDirectorySeparator(ref string sMsg)
+        {
+            Console.WriteLine("EndsInDirectorySeparator:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetDirectoryName(ref string sMsg)
+        {
+            Console.WriteLine("GetDirectoryName:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetExtension(ref string sMsg, ref string _path)
+        {
+            Console.WriteLine("GetExtension:{0}", sMsg);
+            _path = Path.GetExtension(sMsg);
+            return true;
+        }
+
+        public bool _GetFileName(ref string sMsg)
+        {
+            Console.WriteLine("GetFileName:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetFileNameWithoutExtension(ref string sMsg)
+        {
+            Console.WriteLine("GetFileNameWithoutExtension:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetFullPath(ref string sMsg)
+        {
+            Console.WriteLine("GetFullPath:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetInvalidFileNameChars(ref string sMsg)
+        {
+            Console.WriteLine("GetInvalidFileNameChars:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetInvalidPathChars(ref string sMsg)
+        {
+            Console.WriteLine("GetInvalidPathChars:{0}", sMsg);
+            return true;
+        }
+
+        public bool _GetPathRoot(ref string sMsg)
+        {
+            Console.WriteLine("GetPathRoot:{0}", sMsg);
+            return true;
+        }
+        public bool _GetRandomFileName(ref string sMsg)
+        {
+            Console.WriteLine("GetRandomFileName:{0}", sMsg);
+            return true;
+        }
+        public bool _GetRelativePath(ref string sMsg)
+        {
+            Console.WriteLine("GetRelativePath:{0}", sMsg);
+            return true;
+        }
+        public bool _GetTempFileName(ref string sMsg)
+        {
+            Console.WriteLine("GetTempFileName:{0}", sMsg);
+            return true;
+        }
+        public bool _GetTempPath(ref string sMsg)
+        {
+            Console.WriteLine("GetTempPath:{0}", sMsg);
+            return true;
+        }
+        public bool _HasExtension(ref string sMsg)
+        {
+            Console.WriteLine("HasExtension:{0}", sMsg);
+            return true;
+        }
+        public bool _IsPathFullyQualified(ref string sMsg)
+        {
+            Console.WriteLine("IsPathFullyQualified:{0}", sMsg);
+            return true;
+        }
+        public bool _IsPathRooted(ref string sMsg)
+        {
+            Console.WriteLine("IsPathRooted:{0}", sMsg);
+            return true;
+        }
+        public bool _Join(ref string sMsg)
+        {
+            Console.WriteLine("Join:{0}", sMsg);
+            return true;
+        }
+        public bool _TrimEndingDirectorySeparator(ref string sMsg)
+        {
+            Console.WriteLine("TrimEndingDirectorySeparator:{0}", sMsg);
+            return true;
+        }
+        public bool _TryJoin(ref string sMsg)
+        {
+            Console.WriteLine("TryJoin:{0}", sMsg);
+            return true;
+        }
+
+    }
+}
