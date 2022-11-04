4144a27010ad5ba7ee7f118f7a499aa027fae5fa
ywang43 <james.frosty3150102@gmail.com>
2022-11-04 13:08:46 +0800

    Add FileInfoClass

---
+++ FileInfoClass_CS/FileInfoClass1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileInfoClassLibrary1
+{
+    public class FileInfoClass1
+    {
+        public bool dllTestAPI(ref string sMsg)
+        {
+            Console.WriteLine("dllTestAPI:{0}", sMsg);
+            return true;
+        }
+
+        public bool _AppendText(ref string sMsg)
+        {
+            Console.WriteLine("AppendText:{0}", sMsg);
+            return true;
+        }
+    }
+
+//FileInfo
+    //AppendText
+    //CopyTo
+    //Create
+    //CreateText
+    //Decrypt
+    //Delete
+    //Encrypt
+    //MoveTo
+    //Open
+    //OpenRead
+    //OpenText
+    //OpenWrite
+    //Replace
+    //ToString
+
+}
---
+++ FileInfoClass_CS/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FileInfoClassLibrary1;
+
+namespace FileInfoMethodTests
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfoClassLibrary1.FileInfoClass1 fileInfoClass1 = new FileInfoClassLibrary1.FileInfoClass1();
+            bool _result = false;
+            string _data = "";
+            _result = fileInfoClass1._AppendText(ref _data);
+            if (_result)
+            {
+                Console.WriteLine("true");
+            }
+            
+
+        }
+    }
+}
