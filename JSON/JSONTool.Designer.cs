3c00f856972d0ff11ffb53378acfd337d23d1888
ywang43 <james.frosty3150102@gmail.com>
2023-03-18 10:23:53 +0800

    JSON

---
+++ JSON/JSONTool.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TestPlanCreateJSON
+{
+    partial class JSONTool
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTPtoJSON = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTPtoJSON
+            // 
+            this.btnTPtoJSON.Location = new System.Drawing.Point(508, 33);
+            this.btnTPtoJSON.Name = "btnTPtoJSON";
+            this.btnTPtoJSON.Size = new System.Drawing.Size(106, 46);
+            this.btnTPtoJSON.TabIndex = 0;
+            this.btnTPtoJSON.Text = "TestPlan to JSON";
+            this.btnTPtoJSON.UseVisualStyleBackColor = true;
+            this.btnTPtoJSON.Click += new System.EventHandler(this.btnTPtoJSON_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(471, 490);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(463, 461);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(491, 475);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // JSONTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 515);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTPtoJSON);
+            this.Name = "JSONTool";
+            this.Text = "TestPlan To JSON";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTPtoJSON;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ JSON/JSONTool.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using TPActionDll;
+
+namespace TestPlanCreateJSON
+{
+    public partial class JSONTool : Form
+    {
+        //static int g_testCount = 0;//記錄CSV中共有多少筆
+        //SBSData[] g_SBSData = new SBSData[1];//用來讀入config_SBS文件中的定義
+        //TPActionDll.ActionDll ActionDll = new ActionDll();
+
+        struct SBSData
+        {
+            public string Catalog;
+            public string ID;
+            public string Function;
+            public string DeviceAddress;
+            public string Channel;
+            public string Output;
+            public string Voltage;
+            public string Current;
+            public string Log;
+            public string RunIfFail;
+
+            public SBSData(string m_catalog, string m_id,string m_function,string m_deviceaddress,string m_channel,string m_output,string m_voltage,string m_current,string m_log,string m_runiffail)
+            {
+                this.Catalog = m_catalog;
+                this.ID = m_id;
+                this.Function = m_function;
+                this.DeviceAddress = m_deviceaddress;
+                this.Channel = m_channel;
+                this.Output = m_output;
+                this.Voltage = m_voltage;
+                this.Current = m_current;
+                this.Log = m_log;
+                this.RunIfFail = m_runiffail;
+            }
+        }
+        public JSONTool()
+        {
+            InitializeComponent();
+            //GetSBSConfigDataCount();
+            #region ComboBox1
+            comboBox1.Items.Add("OV1");
+            comboBox1.Items.Add("OV2");
+            comboBox1.Items.Add("OV3");
+            comboBox1.Items.Add("UV1");
+            comboBox1.Items.Add("UV2");
+            comboBox1.Items.Add("UV3");
+            comboBox1.Items.Add("OTC1");
+            comboBox1.Items.Add("OTC2");
+            comboBox1.Items.Add("OTC3");
+            comboBox1.Items.Add("UTC1");
+            comboBox1.Items.Add("UTC2");
+            comboBox1.Items.Add("UTC3");
+            comboBox1.Items.Add("OTD1");
+            comboBox1.Items.Add("OTD2");
+            comboBox1.Items.Add("OTD3");
+            comboBox1.Items.Add("OTD4");
+            comboBox1.Items.Add("UTD1");
+            comboBox1.Items.Add("UTD2");
+            comboBox1.Items.Add("UTD3");
+            comboBox1.Items.Add("FET_OT-1");
+            comboBox1.Items.Add("FET_OT-2");
+            comboBox1.Items.Add("FET_OT-3");
+            comboBox1.Items.Add("FET_OT-4");
+            comboBox1.Items.Add("CFETF");
+            comboBox1.Items.Add("DFETF");
+            comboBox1.Items.Add("SUV");
+            comboBox1.Items.Add("SOV");
+            comboBox1.Items.Add("UTC3noCHG");
+            comboBox1.Items.Add("CFETFFuse");
+            comboBox1.Items.Add("DFETFFuse");
+            #endregion ComboBox1
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class FDK3KWJson
+        {
+            //public string Catalog { get; set; }
+            //public string ID { get; set; }
+            //public Items items;
+            public IList<Items> ItemName { get; set; }
+            public IList<Items> ItemName2 { get; set; }
+            public IList<Items> ItemName3 { get; set; } 
+        }
+        public class Items
+        {
+            public string ItemNo;
+            public string Enable;
+            public string Description;
+            //public List<string> Procedure = new List<string> { };
+            //public IList<toolSetting> Procedure2 { get; set; }        //<要填class 名稱>
+            public List<toolSetting> Procedure = new List<toolSetting>
+            {
+                new toolSetting{Catalog = "Relay",ID = "R0001",Function = "SetChannel",CellNTCCurrent = "Cell1",PFFuse = "PF",Output = "On",Time = null,Log = "Y",RunIfFail = "Y"}, //先切換 Cell1,NTCTS5,NTCTS1,Current
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="1",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="2",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="3",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="4",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "Function",ID = "Func501",Time = "15000",Log = "Y",RunIfFail = "Y"},
+            };
+            public List<toolSetting> Procedure2 = new List<toolSetting>
+            {
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="1",Output = "On",Voltage = "4100",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="2",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="3",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="4",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+            };
+            public List<toolSetting> Procedure3 = new List<toolSetting>
+            {
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="1",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="2",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="3",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+                new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1001",Function= "SetVoltage(V)",DeviceAddress="7",Channel="4",Output = "On",Voltage = "4000",Current = "3000",Log = "Y",RunIfFail="Y",},
+            };
+        }
+
+        public class toolSetting
+        {
+            public string Catalog { get; set; }
+            public string ID { get; set; }
+            public string Function { get; set; }
+            public string DeviceAddress { get; set; }
+            public string Channel { get; set; } //Power Supply Channel
+            public string Output { get; set; }
+            public string Voltage { get; set; }
+            public string Current { get; set; }
+            public string Log { get; set; }
+            public string RunIfFail { get; set; }
+            public string Time { get; set; }
+            public string PFFuse{get;set;}
+            public string CellNTCCurrent{get;set;}
+        }
+#if debug
+        private void GetSBSConfigDataCount()
+        {
+            StreamReader _reader = new StreamReader(Application.StartupPath + "\\" + "Config_createJSON.csv");
+            while (!_reader.EndOfStream)
+            {
+                _reader.ReadLine();
+                g_testCount++;
+            }
+            g_testCount = g_testCount - 1;//減去標頭
+            Array.Resize(ref g_SBSData, g_testCount);
+            _reader.Close();
+            _reader.Dispose();
+        }
+#endif
+
+        private void btnTPtoJSON_Click(object sender, EventArgs e)
+        {
+            var fdk3kw = new FDK3KWJson
+            {
+                ItemName = new List<Items>  //正常波型
+                {
+                    new Items{ItemNo = "0001",Enable = "Y",Description ="OV1",
+                        //Procedure= new List<toolSetting>
+                        //{             
+                        //},
+                    }
+                },
+                ItemName2 = new List<Items> //Trigger波型
+                {
+                    new Items{ItemNo = "002",Enable = "Y",Description ="OV2", 
+                        Procedure= new List<toolSetting>
+                        {                       
+                        },
+                        Procedure2= new List<toolSetting>
+                        {                       
+                        },
+                        Procedure3= new List<toolSetting>
+                        {                       
+                        },
+                    }
+                },
+                ItemName3 = new List<Items> //Release 波型
+                {
+                    new Items{ItemNo = "003",Enable = "Y",Description ="OV3", 
+                        Procedure2= new List<toolSetting>
+                        {      
+                            new toolSetting{Catalog = "4ChPowerSupply",ID = "4P1003",Function= "OV3Release",DeviceAddress="7",Channel="3",Output = "On",},
+                        },
+                    }
+                },
+
+                //items = new Items
+                //{
+                //    ItemNo = "0001",
+                //    Enable = "Y",
+                //    Description = "OV1",   //測試名稱
+                //    Procedure = new List<toolSetting>
+                //    {
+                //     new toolSetting{Catalog = "",ID = "",Function = "",DeviceAddress = "",Channel = "",Output="",Voltage= "",Current="",Log = "",RunIfFail= ""}
+                //  }
+                //},
+
+            };
+            string fileName = "FDK3KW.json";
+            //string jsonString = JsonConvert.SerializeObject(fdk3kw);
+            string jsonString = JsonConvert.SerializeObject(fdk3kw,Formatting.Indented,new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            Console.WriteLine(jsonString);
+            File.WriteAllText((Application.StartupPath + "//" + fileName), jsonString);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data = "George of the jungle";
+            //ActionDll.dlltestAPI(ref data);
+        }
+    }
+}
