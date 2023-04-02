8679e8ea55262e03846fd5e5a704c5fd3aacc429
ywang43 <james.frosty3150102@gmail.com>
2023-04-02 12:28:11 +0800

    JamesPlatform

---
+++ JamesPlatform/JamesPlatform.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace JamesPlatform
+{
+    partial class JamesPlatform
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Location = new System.Drawing.Point(620, 46);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(121, 52);
+            this.btnPatch.TabIndex = 0;
+            this.btnPatch.Text = "Patch";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // JamesPlatform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPatch);
+            this.Name = "JamesPlatform";
+            this.Text = "JamesPlatForm";
+            this.Load += new System.EventHandler(this.JamesPlatform_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPatch;
+    }
+}
+
---
+++ JamesPlatform/JamesPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace JamesPlatform
+{
+    public partial class JamesPlatform : Form
+    {
+        public JamesPlatform()
+        {
+            InitializeComponent();
+        }
+
+        private void JamesPlatform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
