135e4a3a5d9049f21c261381120d7f1b608a8539
ywang43 <james.frosty3150102@gmail.com>
2023-04-22 18:24:12 +0800

    SOC table OK

--- soc_table/Form1.Designer.cs
+++ soc_table/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -41,15 +43,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(571, 358);
+            this.dataGridView1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "SOC table";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- soc_table/Form1.cs
+++ soc_table/Form1.cs
@@ -25,14 +25,17 @@ namespace soc_table
         bool _percenthundred = false;
         string[] PercentageVoltage = new string[105];
         string _prefix = "";
-        //string g_filepath = Application.StartupPath + "\\" + _prefix + ".csv";
+        string g_filepath = Application.StartupPath + "\\" + "soc_table"+ ".csv";
         string[] _criticalPercentage = new string[1];
         int _count = 0;
         int _clearPercent = 0;
         double _voltage = 0.0;
-        int soc_unitsdigit = 0; //個位數
-        int soc_rec_unitsdigit = 0; //個位數
-        int soc_tensdigits = 0; //十位數
+        string soc_unitsdigit = ""; //個位數
+        string soc_rec_unitsdigit = ""; //個位數
+        string soc_tensdigits = ""; //十位數
+        int _voltageInit = 0;
+        //DataGridView 
+        int colon = 0;
 
         public Form1()
         {
@@ -61,45 +64,90 @@ namespace soc_table
                 switch (_dot)
                 { 
                     case -1:    //沒有100%, 0%
-                        _criticalPercentage[0] = _soc_double.ToString("0") + ":" +_voltage;
-                        Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
-                        _count += 1;
-                        //_percenthundred = percentCapHundred(_soc);
+                        if ((_soc_double.ToString() == "100") || (_soc_double.ToString() == "0"))    //削除多一個50%的誤動作
+                        {
+                            _criticalPercentage[0] = _soc_double.ToString("0") + "%:" + _voltage;
+                            Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
+                            _count += 1;
+                        }
                         break;
                     case 2:
-                        //soc_unitsdigit = _soc.Substring(0,_soc)
+                        soc_unitsdigit = _soc_double.ToString().Substring((_dot-1), 1);   //找個位數
+                        soc_rec_unitsdigit = _soc_rec_double.ToString().Substring((_dot-1),1);
                         if (_dot == _dot_rec)    //確認小數位數相同
                         {
-                            //if () //判斷變動位
-                            //{ 
-
-                            //}
+                            if (soc_unitsdigit != soc_rec_unitsdigit) //判斷變動位
+                            {
+                                Console.WriteLine(_soc);
+                                _criticalPercentage[0] = _soc_double.ToString().Substring(0, _dot) + "%:" + _voltage;
+                                Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
+                                _count += 1;
+                            }
                         }
                         else if ((_dot == 2) && (_dot_rec == -1))   //99%
                         {
-                            _criticalPercentage[0] = _soc_double.ToString().Substring(0, _dot) + ":" + _voltage;
+                            _criticalPercentage[0] = _soc_double.ToString().Substring(0, _dot) + "%:" + _voltage;
                             Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
                             _count += 1;
                             //_percenthundred = percentCapHundred(_soc);
                             break;
                         }
-#if debug
-                        _criticalPercentage[0] = _soc + ":" + (double.Parse(_tempArray[0]) * 1000).ToString();
-                        Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
-                        _count += 1;
-#endif
-                        //_percentage = percentCap();
                         break;
                     case 1:
                         Console.WriteLine("_soc:{0}", _soc);
                         Console.WriteLine("_soc_rec:{0}", _soc_rec);
-                        break;
+                        soc_unitsdigit = _soc_double.ToString().Substring((_dot - 1), 1);   //找個位數
+                        soc_rec_unitsdigit = _soc_rec_double.ToString().Substring((_dot-1), 1);
+                        if (_dot == _dot_rec)    //確認小數位數相同
+                        {
+                            if (soc_unitsdigit!= soc_rec_unitsdigit)
+                            {
+                                if (soc_unitsdigit != "0")
+                                {
+                                    _criticalPercentage[0] = _soc_double.ToString().Substring(0, _dot) + "%:" + _voltage;
+                                    Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
+                                    _count += 1;
+                                    //_percenthundred = percentCapHundred(_soc);
+                                    //break;                              
+                                }
+                            }
+                        }
+                        else if ((_dot == 1) && (_dot_rec == 2))   //9%
+                        {
+                            _criticalPercentage[0] = _soc_double.ToString().Substring(0, _dot) + "%:" + _voltage;
+                            Array.Copy(_criticalPercentage, 0, PercentageVoltage, _count, 1);
+                            _count += 1;
+                            //_percenthundred = percentCapHundred(_soc);
+                            //break;
+                        }
+                            break;
 
                 }
                 _soc_rec = _soc;
                 _soc_rec_double = _soc_double;
 
             }
+
+            File.WriteAllLines(g_filepath, PercentageVoltage);
+            #region Table
+            DataTable dt = new DataTable("table");
+            dt.Columns.Add("Percent(%)");
+            dt.Columns.Add("Voltage");
+            DataRow NewRow;
+            for (int i = 0; i < PercentageVoltage.Length; i++)
+            {
+                if (PercentageVoltage[i] != null)
+                {
+                    NewRow = dt.NewRow();
+                    _voltageInit = PercentageVoltage[i].LastIndexOf(':');
+                    NewRow["Percent(%)"] = PercentageVoltage[i].Substring(0, _voltageInit);
+                    NewRow["Voltage"] = PercentageVoltage[i].Substring((_voltageInit + 1), (PercentageVoltage[i].Length - (_voltageInit + 1)));
+                    dt.Rows.Add(NewRow);
+                }
+            }
+            dataGridView1.DataSource = dt;
+            #endregion
+            File.WriteAllLines(g_filepath, PercentageVoltage);
         }
 
         private void Form1_Load(object sender, EventArgs e)
