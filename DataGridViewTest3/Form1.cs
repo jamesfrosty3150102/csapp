bde5691c0fe547420b5720219398d0a1befc6f93
ywang43 <james.frosty3150102@gmail.com>
2023-08-03 11:56:26 +0800

    DataGridViewTest3

---
+++ DataGridViewTest3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewTest3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataGridView DGV = new DataGridView();
+            DGV.Dock = DockStyle.Fill;
+            DGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.Controls.Add(DGV);
+            DataTable dataTable = new DataTable();
+            for (int i = 0; i < 16; i++)
+            {
+                dataTable.Columns.Add("Bit" + i.ToString(), typeof(byte));
+            }
+            DGV.DataSource = dataTable;
+
+        }
+    }
+}
